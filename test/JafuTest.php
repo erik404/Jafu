c11bf5ede8f91ff0ae10961ab009b6757148ac7e
erik404 <ejvandewal@gmail.com>
2017-05-04 03:39:57 +0200

    Travis  bug test

--- test/JafuTest.php
+++ test/JafuTest.php
@@ -1,5 +1,5 @@
 <?php
-require 'vendor/autoload.php';
+require '../vendor/autoload.php';
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -7,7 +7,7 @@ use PHPUnit\Framework\TestCase;
  */
 final class JafuTest extends TestCase
 {
-    const CONFIG_DIST_FILE = 'src/erik404/config.dist.php';
+    const CONFIG_DIST_FILE = '../src/erik404/config.dist.php';
 
     /**
      * Test config.dist.php
