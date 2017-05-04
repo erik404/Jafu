7fe8e22f2c78401d741e49684f2ec4f4e00aa0fa
erik404 <ejvandewal@gmail.com>
2017-05-04 02:45:29 +0200

    added various testcases

--- tests/JafuTest.php
+++ tests/JafuTest.php
@@ -10,9 +10,6 @@ final class JafuTest extends TestCase
 {
     const CONFIG_DIST_FILE = '../src/erik404/config.dist.php';
 
-    private $configFile;
-    private $JafuTest;
-
     /**
      * Test config.dist.php
      */
@@ -24,12 +21,35 @@ final class JafuTest extends TestCase
         $configFile = require JafuTest::CONFIG_DIST_FILE;
         // assert if the $configFile holds an object
         $this->assertEquals(true, gettype($configFile) === 'object', "config.dist.php should translate to object.");
-        // assert if defaultSaveLocation holds a string
-        $this->assertEquals(true, gettype($configFile->defaultSaveLocation) === 'string', "config.dist.php defaultSaveLocation should hold string.");
-        // assert if responseMessages holds an array
-        $this->assertEquals(true, gettype($configFile->responseMessages) === 'array', "config.dist.php responseMessages should hold array.");
-        // passed tests, set $configFile to class scope
-        $this->configFile = $configFile;
+        // assert if config object has defaultSaveLocation of type string
+        $this->assertObjectHasAttribute('defaultSaveLocation', $configFile, 'config object should have "defaultSaveLocation" attribute.');
+        $this->assertEquals(true, gettype($configFile->defaultSaveLocation) === 'string', "config->defaultSaveLocation should hold string.");
+        // assert if config object has responseMessages of type array
+        $this->assertObjectHasAttribute('responseMessages', $configFile, 'config object should have "responseMessages" attribute.');
+        $this->assertEquals(true, gettype($configFile->responseMessages) === 'array', "config->responseMessages should hold array.");
     }
+
+    /**
+     * Test if the class can be instantiated and is instance of erik404\Jafu
+     *
+     * @depends testConfigDist
+     */
+    public function testJafuClass()
+    {
+        $jafu = new \erik404\Jafu(require JafuTest::CONFIG_DIST_FILE);
+        $this->assertInstanceOf(\erik404\Jafu::class, $jafu);
+    }
+
+    /**
+     * Test the getDefaultSaveLocation method
+     *
+     * @depends testJafuClass
+     */
+    public function testIfDefaultSaveLocationCanBeRetrieved()
+    {
+        $jafu = new \erik404\Jafu(require JafuTest::CONFIG_DIST_FILE);
+        $this->assertEquals(true, gettype($jafu->getDefaultSaveLocation()) === 'string');
+    }
+
 
 }
