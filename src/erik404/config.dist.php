1255b8727bb4811e2db9742dd6d74f83e04bb489
erik404 <ejvandewal@gmail.com>
2017-05-04 02:58:33 +0200

    changed defaultSaveLocation path for Travis test

--- src/erik404/config.dist.php
+++ src/erik404/config.dist.php
@@ -1,5 +1,5 @@
 <?php return (object)array(
-    'defaultSaveLocation' => __DIR__ . '\\',
+    'defaultSaveLocation' => __DIR__ . '/',
     'responseMessages'    => array(
         0  => 'There is no error, the file uploaded with success.',
         1  => 'The uploaded file exceeds the upload_max_filesize directive in php.ini.',
