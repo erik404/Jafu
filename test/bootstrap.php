e8fe9f2aff5136d8c834d31dff2914f582ac7e39
erik404 <ejvandewal@gmail.com>
2017-05-05 06:04:51 +0200

    added explaining comment for overwritten function.

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,22 +1,20 @@
 <?php
 namespace erik404;
 
-/*
- * Set error reporting to the level to which Es code must comply.
- */
 error_reporting(E_ALL | E_STRICT);
-/*
- * Determine the root, library, and tests directories of the framework
- * distribution.
- */
+
 require(__DIR__ . '/../src/erik404/Jafu.php');
 require __DIR__ . '/../vendor/autoload.php';
 
 /**
- * Overwrite here php functions
+ * Overwrite move_uploaded_file because this function is safe and therefor gives an error with PHPUnit test.
+ * Use the unsafe copy function for testing purpose.
+ *
+ * @param $filename
+ * @param $destination
+ * @return bool
  */
 function move_uploaded_file($filename, $destination)
 {
-    //Copy file
     return copy($filename, $destination);
 }
