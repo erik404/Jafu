9752fa19bdaa883340d42fe7102b7b74fe4e9dbb
erik404 <ejvandewal@gmail.com>
2017-05-05 02:37:36 +0200

    refactoring

--- src/erik404/config.dist.php
+++ src/erik404/config.dist.php
@@ -1,6 +1,6 @@
-<?php return (object)array(
-    'defaultSaveLocation' => __DIR__ . '/',
-    'responseMessages'    => array(
+<?php return (object)[
+    'defaultSaveLocation' => __DIR__.'/',
+    'responseMessages'    => [
         0  => 'There is no error, the file uploaded with success.',
         1  => 'The uploaded file exceeds the upload_max_filesize directive in php.ini.',
         2  => 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form.',
@@ -11,5 +11,5 @@
         8  => 'A PHP extension stopped the file upload.',
         9  => 'File type %s not allowed.',
         10 => 'MIME validation via finfo failed.'
-    )
-);
+    ]
+];
